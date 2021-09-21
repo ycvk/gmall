c3c162222f76635b47f6e8d8929259bf813e91b5
ycvk <ycvk@qq.com>
2021-09-21 23:13:18 +0800

    nginx做静态代理方式生成首页静态页面

--- web/web-all/src/main/java/com/atguigu/gmall/web/weball/controller/IndexController.java
+++ web/web-all/src/main/java/com/atguigu/gmall/web/weball/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.atguigu.gmall.web.weball.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.product.client.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -24,6 +29,9 @@ public class IndexController {
     @Autowired
     private ProductFeignClient productFeignClient;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @GetMapping({"/", "index.html"})
     public String index(Model model) {
 
@@ -31,5 +39,28 @@ public class IndexController {
         model.addAttribute("list", baseCategoryList);
 
         return "index/index";
+    }
+
+    /**
+     * nginx做静态代理方式生成静态页面
+     */
+    @GetMapping("createIndex")
+    public Result createIndex() {
+        //  获取后台存储的数据
+        List<JSONObject> baseCategoryList = productFeignClient.getBaseCategoryList();
+        //  设置模板显示的内容
+        Context context = new Context();
+        context.setVariable("list", baseCategoryList);
+
+        //  定义文件输入位置
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("D:\\index.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //  调用process();方法创建模板
+        templateEngine.process("index/index.html", context, fileWriter);
+        return Result.ok();
     }
 }
